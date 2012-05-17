199ceff9b43f5d27ee6a0f8943654664b5458acc
Saul Cozens <me@saulcozens.co.uk>
2012-05-17 11:19:25 +0100

    fixed a typo that meant it ignore the since_id

--- listhashtag.py
+++ listhashtag.py
@@ -1,43 +1,49 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+import os
+from google.appengine.ext.webapp import template
 from google.appengine.api import urlfetch
 import urllib
 from urllib import unquote
 from django.utils import simplejson as json
 import re
-
-
+from twitter_oauth_handler import *
+   
 
 class CheckList(webapp.RequestHandler):
 	def get(self, username, listname, hashtag, since_id=None):
+		
+		client = OAuthClient('twitter', self)
+		
 		matched=False
-		url = "https://api.twitter.com/1/lists/statuses.json?owner_screen_name="+username+"&slug="+listname
-		if (since_id != None) :
-			url += "&since_id="+since_id
+		url = "https://api.twitter.com/1/lists/statuses.json"
 		hashtag_re = "#("+'|'.join(unquote(hashtag).split(',')) + ")"
 		self.response.headers['Content-Type'] = 'text/plain'
-		resp = urlfetch.fetch(url=url,
-								method=urlfetch.GET)
-		if resp.status_code == 200 :
-			tweets=json.loads(resp.content)
-			if len(tweets) > 0 :
-				since_id = tweets[0]['id']
-				for tweet in tweets:
-					if re.search( hashtag_re, tweet["text"], re.I) :
-						matched = True
-						break
+		if (since_id != None) :
+			tweets=client.get(url, (200,), owner_screen_name=username, slug=listname, since_id=since_id)
 		else:
-			self.response.set_status(500, "Something went wrong. Probably something to do with the twitter API call. Somebody should probably do something about it")
+			tweets=client.get(url, (200,), owner_screen_name=username, slug=listname)
 
+		if len(tweets) > 0 :
+			since_id = tweets[0]['id']
+			for tweet in tweets:
+				if re.search( hashtag_re, tweet["text"], re.I) :
+					matched = True
+					break
 
 		self.response.out.write([matched,str(since_id)])
 			
 
+class IndexPage(webapp.RequestHandler):
+		def get(self) :
+			path = os.path.join(os.path.dirname(__file__), 'index.html')
+			self.response.out.write(template.render(path, {}))		
 
-application = webapp.WSGIApplication(
-									[(r'/([^/]*)/([^/]*)/([^/]*)', CheckList),  ## username, listname, hashtags
-                   (r'/([^/]*)/([^/]*)/([^/]*)/(\d+)', CheckList)],  ## username, listname, hashtags, since_id
-									debug=True)
+application = webapp.WSGIApplication([(r'/oauth/(.*)/(.*)', OAuthHandler),
+                                      (r'/[^/]*', IndexPage),
+                                      (r'/([^/]*)/([^/]*)/([^/]*)', CheckList),  ## username, listname, hashtags
+                                      (r'/([^/]*)/([^/]*)/([^/]*)/(\d+)', CheckList)],  ## username, listname, hashtags, since_id
+                                     debug=True)
 
 def main():
 	run_wsgi_app(application)
